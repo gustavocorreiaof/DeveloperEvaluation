a0ee567d8295439d018f3c979713d563a4db76fa
Gustavo 紫 <52300498+gustavocorreiaof@users.noreply.github.com>
2025-05-25 20:54:33 -0300

    feat: login page

--- GlobalClimateAPI/GlobalClimateAPI/Program.cs
+++ GlobalClimateAPI/GlobalClimateAPI/Program.cs
@@ -89,8 +89,21 @@ builder.Services.AddAuthentication(options =>
     };
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
+
+
 var app = builder.Build();
 
+app.UseCors("AllowAll");
 app.UseSwagger();
 app.UseSwaggerUI(options =>
 {
